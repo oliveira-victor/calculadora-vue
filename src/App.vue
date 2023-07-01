1189f20fb19ab555a6f64fc36ea3c8678b2c9377
victor <victor.freire.oliveira@gmail.com>
2023-06-30 23:07:00 -0300

    interface improvement

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,83 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const bgColor = () => {
+    document.querySelector('body').style.background = '#494949'
+}
+bgColor()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div class="container">
+    <header>
+        <h1>Calculadora</h1>
+    </header>
+    <div class="main">
+        <form>
+            <input required type="number" placeholder="0">
+            <input required type="number" placeholder="0">
+            <select class="select">
+                <option value="adicao">Adição +</option>
+                <option value="subtração">Subtração -</option>
+                <option value="multiplicacao">Multiplicação *</option>
+                <option value="divisao">Divisão /</option>
+            </select>
+        </form>
+        <div class="results">
+            O resultado da conta é:<br />
+            <div class="number">
+                0
+            </div>
+        </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+</div>
 </template>
 
 <style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 24px;
+}
+.container {
+    width: 800px;
+    margin: 10% auto;
+    text-align: center;
+    box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.7);
+    border-radius: 40px;
+}
 header {
-  line-height: 1.5;
+    background-color: #c4c4c4;
+    padding: 16px;
+    border-radius: 40px 40px 0 0;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.main {
+    background-color: #e2e2e2;
+    border-radius: 0 0 40px 40px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+form {
+    padding-top: 40px;
+}
+input  {
+    width: 120px;
+    height: 40px;
+    padding: 6px;
+    margin: 6px;
+    border-radius: 8px;
+}
+.select {
+    width: 140px;
+    height: 40px;
+    margin: 6px;
+    border-radius: 8px;
+}
+.results {
+    margin-top: 40px;
+}
+.number {
+    font-size: 46px;
+    font-weight: bold;
+    margin-top: 10px;
+    padding-bottom: 40px;
 }
 </style>
